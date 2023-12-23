376ececae499539e37990e894bfbeef9bc9e65eb
spoonty <sergey.kachalov.02@gmail.com>
2023-12-23 18:24:33 +0700

    minor refactoring of test graph code

--- src/features/graph/Graph.tsx
+++ src/features/graph/Graph.tsx
@@ -28,10 +28,16 @@ const Graph: FC<Props> = ({ nodes, links }) => {
     const width = 1550
     const height = 700
 
+    const color = d3.scaleOrdinal(d3.schemeCategory10)
+
     const svg = d3
       .select(svgRef.current)
       .attr('width', width)
       .attr('height', height)
+      .attr('viewBox', [-width / 2, -height / 2, width, height])
+      .attr('style', 'max-width: 100%; height: auto;')
+
+    svg.selectAll('*').remove()
 
     const simulation = d3
       .forceSimulation(nodes)
@@ -40,59 +46,43 @@ const Graph: FC<Props> = ({ nodes, links }) => {
         d3
           .forceLink(links)
           .id((d: any) => d.id)
-          .distance(400),
+          .distance(200),
       )
-      // .force('charge', d3.forceManyBody().strength(-400))
-      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('charge', d3.forceManyBody().strength(0))
+
+    // .force('charge', d3.forceManyBody().strength(-200))
+    // .force('center', d3.forceCenter(width / 2, height / 2));
 
     const link = svg
       .append('g')
-      .selectAll<SVGLineElement, Link>('line')
-      .data(links, (d) => `${d.source}-${d.target}`)
-      .enter()
-      .append('line')
-      .attr('stroke', 'black')
-      .attr('stroke-width', 2)
-      
+      .attr('stroke', '#999')
+      .attr('stroke-opacity', 0.6)
+      .selectAll('line')
+      .data(links)
+      .join('line')
+      .attr('stroke-width', (d: any) => Math.sqrt(d.value))
+
     const node = svg
       .append('g')
-      .selectAll<SVGCircleElement, Node>('circle')
-      .data(nodes, (d) => `${d.id}`)
-      .enter()
-      .append('circle')
-      .attr('r', 40)
-      .attr('fill', 'red') // Устанавливаем красный цвет для заливки вершины
-      .attr('stroke', 'black') // Устанавливаем черный цвет для границы вершины
-      .attr('stroke-width', 2)
-      //@ts-ignore
-      .call(
-        //@ts-ignore
-        d3
-          .drag()
-          .on('start', dragstarted)
-          .on('drag', dragged)
-          .on('end', dragended),
-      )
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 1.5)
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('r', 20)
+      .attr('fill', (d: any) => color(d.group))
 
-    const label = svg
-      .append('g')
-      .selectAll<SVGTextElement, Node>('text')
-      .data(nodes, (d) => `${d.id}`)
-      .enter()
-      .append('text')
-      .text((d) => d.label)
-      .attr('dy', 4)
-      .attr('text-anchor', 'middle')
+    node.append('title').text((d) => d.id)
 
-    const linkLabel = svg
-      .append('g')
-      .selectAll<SVGTextElement, Link>('text')
-      .data(links, (d) => `${d.source}-${d.target}`)
-      .enter()
-      .append('text')
-      .text((d) => d.type)
-      .attr('dy', -5)
-      .attr('text-anchor', 'middle')
+    // @ts-ignore
+    node.call(
+      // @ts-ignore
+      d3
+        .drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended),
+    )
 
     simulation.on('tick', () => {
       link
@@ -101,45 +91,30 @@ const Graph: FC<Props> = ({ nodes, links }) => {
         .attr('x2', (d: any) => d.target.x)
         .attr('y2', (d: any) => d.target.y)
 
-      node.attr('cx', (d) => d.x || 0).attr('cy', (d) => d.y || 0)
-
-      label.attr('x', (d) => d.x || 0).attr('y', (d) => d.y || 0)
-
-      linkLabel
-        .attr('x', (d: any) => (d.source.x + d.target.x) / 2 || 0)
-        .attr('y', (d: any) => (d.source.y + d.target.y) / 2 || 0)
+      node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y)
     })
 
     function dragstarted(event: any) {
       if (!event.active) simulation.alphaTarget(0.3).restart()
-      if (event.subject) {
-        const d = event.subject as Node
-        d.fx = d.x
-        d.fy = d.y
-      }
+      event.subject.fx = event.subject.x
+      event.subject.fy = event.subject.y
     }
 
     function dragged(event: any) {
-      if (event.subject) {
-        const d = event.subject as Node
-        d.fx = event.x
-        d.fy = event.y
-      }
+      event.subject.fx = event.x
+      event.subject.fy = event.y
     }
 
     function dragended(event: any) {
-      if (!event.active) simulation.alphaTarget(0)
-      if (event.subject) {
-        const d = event.subject as Node
-        d.fx = null
-        d.fy = null
-      }
+      // if (!event.active) simulation.alphaTarget(0)
+      event.subject.fx = null
+      event.subject.fy = null
     }
 
     return () => {
       simulation.stop()
     }
-  }, [])
+  }, [nodes, links])
 
   return <svg ref={svgRef} className="border-2 border-black"></svg>
 }
