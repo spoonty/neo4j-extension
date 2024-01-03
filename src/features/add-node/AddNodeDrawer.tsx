14757dbeb039374a8ebddeed5242b46a1b2a9cda
spoonty <sergey.kachalov.02@gmail.com>
2024-01-03 13:05:40 +0700

    create clue component

--- src/features/add-node/AddNodeDrawer.tsx
+++ src/features/add-node/AddNodeDrawer.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react'
+import Clue from '@/ui/Clue'
 import { Content, Drawer, Footer, Header } from '@/ui/Drawer'
 import PopoverInput from '@/ui/Input/PopoverInput'
 import Stepper from '@/ui/Stepper/Stepper'
@@ -11,7 +12,7 @@ interface Props {
 const AddNodeDrawer: FC<Props> = ({ open, onClose }) => {
   const [step, setStep] = useState(0)
 
-  const steps = ['Select Label', 'Set Properties']
+  const steps = ['Set Labels', 'Set Properties']
 
   return (
     <Drawer open={open} modal={false}>
@@ -19,8 +20,12 @@ const AddNodeDrawer: FC<Props> = ({ open, onClose }) => {
         <Header onClose={onClose}>CREATE NODE</Header>
         <div className="my-4 flex flex-col gap-5">
           <Stepper steps={steps} current={step} />
-          <div className="px-2">
+          <div className="flex flex-col gap-2 px-2">
             <PopoverInput placeholder="Label" />
+            <Clue>
+              Node labels represent tags assigned to graph nodes for
+              categorizing them by meaning or functionality.
+            </Clue>
           </div>
         </div>
         <Footer></Footer>
---
+++ src/ui/Clue.tsx
@@ -0,0 +1,25 @@
+import { FC, PropsWithChildren } from 'react'
+import QuestionIcon from '@/assets/icons/QuestionIcon'
+import { cn } from '@/utils/dom'
+
+interface Props extends PropsWithChildren {
+  className?: string
+}
+
+const Clue: FC<Props> = ({ children, className }) => (
+  <div
+    className={cn(
+      'flex cursor-default items-center text-sm leading-4 text-text-gray',
+      className,
+    )}
+  >
+    <QuestionIcon
+      width="20"
+      height="20"
+      className="me-3 inline h-4 w-4 flex-shrink-0"
+    />
+    <span>{children}</span>
+  </div>
+)
+
+export default Clue
